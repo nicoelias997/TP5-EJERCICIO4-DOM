6a3cde445ec5659503dfb6c17cbfbb03be233589
nicoelias997 <nicoelias997@gmail.com>
2022-08-06 17:06:29 +0200

    Termine el reloj

---
+++ js/ejercicio4.js
@@ -0,0 +1,50 @@
+const fecha = document.getElementById("fecha");
+const hora = document.getElementById("hora");
+const box2 = document.getElementById("box2")
+
+const meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+const dias = ['Domingo', 'Lunes', 'martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+
+    let fechaActual = new Date();
+    fecha.className = "fs-3 py-3 text-center text-white"
+    fecha.innerHTML = `${dias[fechaActual.getDate()]} ${fechaActual.getDate()} de ${meses[fechaActual.getMonth()]} del ${fechaActual.getFullYear()}`
+
+
+    let horaActual = new Date();
+
+    let formato = "";
+    if(horaActual.getHours() > 12){
+        formato = "PM"
+    } else {
+        formato = "AM"
+    }
+    
+    let segundos;
+    if(horaActual.getSeconds() < 10){
+        segundos = `0${horaActual.getSeconds()}`
+    } else {
+        segundos = horaActual.getSeconds()
+    }
+    
+    let minutos;
+    if(horaActual.getMinutes() < 10){
+        minutos = `0${horaActual.getMinutes()}`
+    } else {
+        minutos = horaActual.getMinutes()
+    }
+
+    let horas;
+    if(horaActual.getHours() < 10){
+        horas = `0${horaActual.getHours()}`
+    } else {
+        horas = horaActual.getHours()
+    }
+
+    hora.className = "d-flex align-items-center"
+    hora.innerHTML = `
+    <div class="d-flex align-items-center justify-content-center col text-center py-5">
+    <p class="fs-1 text-white p-1">${horas} : ${minutos} : </p>
+
+    <p class="fs-5 lh-1 text-white p-1">${formato}<br>${segundos}</p>
+    <div>
+    `
