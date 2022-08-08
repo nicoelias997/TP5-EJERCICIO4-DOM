d71fe729a0741337354d664f4442dc40db30e8a0
nicoelias997 <nicoelias997@gmail.com>
2022-08-08 13:22:03 +0200

    Agrego un setInterval para que se muestre la hora en vivo, y corrijo la fecha, para que una vez que el dia sea mayor a 7, vuelva a contarse el array de dias

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -5,12 +5,20 @@ const box2 = document.getElementById("box2")
 const meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
 const dias = ['Domingo', 'Lunes', 'martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
 
+    let diaSemana;
     let fechaActual = new Date();
-    fecha.className = "fs-3 py-3 text-center text-white"
-    fecha.innerHTML = `${dias[fechaActual.getDate()]} ${fechaActual.getDate()} de ${meses[fechaActual.getMonth()]} del ${fechaActual.getFullYear()}`
+
+    if(dias[fechaActual.getDate()] == undefined){
+        diaSemana = dias[fechaActual.getDate() - 7]
+    }
+
+    fecha.className = "fs-3 py-3 text-center text-white";
+    fecha.innerHTML = `${diaSemana} ${fechaActual.getDate()} de ${meses[fechaActual.getMonth()]} del ${fechaActual.getFullYear()}`
 
 
-    let horaActual = new Date();
+    setInterval(() => {
+
+        let horaActual = new Date();
 
     let formato = "";
     if(horaActual.getHours() > 12){
@@ -39,7 +47,7 @@ const dias = ['Domingo', 'Lunes', 'martes', 'Miércoles', 'Jueves', 'Viernes', '
     } else {
         horas = horaActual.getHours()
     }
-
+    
     hora.className = "d-flex align-items-center"
     hora.innerHTML = `
     <div class="d-flex align-items-center justify-content-center col text-center py-5">
@@ -48,3 +56,6 @@ const dias = ['Domingo', 'Lunes', 'martes', 'Miércoles', 'Jueves', 'Viernes', '
     <p class="fs-5 lh-1 text-white p-1">${formato}<br>${segundos}</p>
     <div>
     `
+    }
+    ,100)
+    
